5fc0aaea3e073c1ed11495430cbdea844a7bbfdc
Taiwo kazeem <kazeem@kobo360.com>
2022-07-21 10:55:16 +0100

    add job to deploy backend and frontend

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,9 @@
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+export TYPEORM_HOST=rds-postgresql.cj0kw3cqu1tq.us-west-2.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=Oluranti08056965
+export TYPEORM_DATABASE=udacityDb
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_MIGRATIONS_DIR=./src/migrations
